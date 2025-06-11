3a9f446f8be83479348ce997cc1fc267933fae7b
Grant Bacon <btnarg+github@gmail.com>
2025-06-11 08:30:08 -0700

    dont comment on my trash

--- src/service/AboutService.ts
+++ src/service/AboutService.ts
@@ -1,16 +1,16 @@
 import { Service } from "./Service";
 import { AboutInfo } from "../types/About";
-import { ApiServiceError } from "../errors/ApiServiceError"; // Import ApiServiceError
+import { ApiServiceError } from "../errors/ApiServiceError";
 
 class AboutService extends Service {
   aboutServer = async (): Promise<AboutInfo> => {
     try {
-      const response = await fetch(this.getAPI() + "/v1/about"); // Added await here
-      if (!response.ok) { // Check if the response is ok
+      const response = await fetch(this.getAPI() + "/v1/about");
+      if (!response.ok) {
         const errorText = await response.text();
         throw new ApiServiceError(errorText, response.status);
       }
-      return response.json() as AboutInfo; // No need for (await response).json() due to await above
+      return response.json() as AboutInfo;
     } catch (e) {
       throw this.unknownError(e);
     }

25f69c79bf99a5614efca38b14a4853ed9d391d7
Grant Bacon <btnarg+github@gmail.com>
2025-06-19 09:02:23 -0700

    Unify MessageService

--- src/service/MessageService.ts
+++ src/service/MessageService.ts
@@ -6,19 +6,21 @@ class MessageService extends RestService {
   sendMessage = async (
     message: SendMessageV2,
   ): Promise<SendMessageResponse> => {
+    let response;
     try {
-      const response = await fetch(this.getAPI() + "/v2/send", {
+      response = await fetch(this.getAPI() + "/v2/send", {
         method: "POST",
         body: JSON.stringify(message),
       });
-      if (!response.ok) {
-        const error = await response.text();
-        throw new ApiServiceError(error, response.status);
-      }
-      return (await response.json()) as SendMessageResponse;
     } catch (e) {
       throw this.unknownError(e);
     }
+
+    if (!response.ok) {
+      const error = await response.text();
+      throw new ApiServiceError(error, response.status);
+    }
+    return (await response.json()) as SendMessageResponse;
   };
 }
 

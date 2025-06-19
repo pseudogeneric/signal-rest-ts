81b1b197702e5fa88612d5ea6d8aabcb71606e1a
Grant Bacon <btnarg+github@gmail.com>
2025-06-19 09:03:45 -0700

    Unify ProfileService

--- src/service/ProfileService.ts
+++ src/service/ProfileService.ts
@@ -4,20 +4,22 @@ import { UpdateProfileRequest } from "../types/Profile";
 
 class ProfileService extends RestService {
   updateProfile = async (
-    number: string,
+    account: string,
     profileUpdate: UpdateProfileRequest,
   ): Promise<void> => {
+    let response;
     try {
-      const response = await fetch(this.getAPI() + "/v1/profiles/" + number, {
+      response = await fetch(this.getAPI() + "/v1/profiles/" + account, {
         method: "PUT",
         body: JSON.stringify(profileUpdate),
       });
-      if (response.status !== 204) {
-        const error = await response.text();
-        throw new ApiServiceError(error, response.status);
-      }
     } catch (e) {
       throw this.unknownError(e);
+    }
+
+    if (response.status !== 204) {
+      const error = await response.text();
+      throw new ApiServiceError(error, response.status);
     }
   };
 }
